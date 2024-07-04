063d2f1de65531b9cee204fde419e352a03d31cc
Jonas Stöcker <jonas.stoecker@gmx.de>
2024-07-04 23:21:48 +0200

    feat: added full support of all usb digital inputs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using HidSharp;
 using System.Collections;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using static ETSSimulator.Program;
 
 namespace ETSSimulator
 {
@@ -116,39 +117,90 @@ namespace ETSSimulator
             var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false);
             _configuration = builder.Build();
 
+            // Check if test mode is selected in appsettings
+            bool testMode = _configuration.GetSection("TestMode").Value == "1";
+
+            if(!testMode)
+            {
+                UserInteraction();
+            }
+
             // List with game control boards
             List<HidDevice> devices = new List<HidDevice>();
 
-            // Start ET2 executable
-            //Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\bin\win_x64\eurotrucks2");
-
+            // Get usb controllers
             while (true)
             {
                 // Search for ETS2 process
-                Process[] processes = Process.GetProcessesByName("WINWORD"); //eurotrucks2
+                Process[] processes = Process.GetProcessesByName("eurotrucks2");
                 Process process = processes.FirstOrDefault();
 
-                // First usb game control board (VID = 121; PID = 6)
-                var device1 = DeviceList.Local.GetHidDeviceOrNull(121, 6);
-                if (device1 != null)
-                    devices.Add(device1);
+                // Get all connected hid devices
+                var usbDevices = DeviceList.Local.GetHidDevices().ToList();
 
-                // Second usb game control board
-                //var device2 = DeviceList.Local.GetHidDeviceOrNull(121, 6);
-                //if (device2 != null)
-                //    devices.Add(device2);
+                // Get number of devices in appsettings
+                int countUsedDevices = (int)_configuration.GetSection("UsbDevices").GetChildren().Count();
+                for (int i = 0; i < countUsedDevices; i++)
+                {
+                    for(int j = 0; j < usbDevices.Count; j++)
+                    {
+                        string devicePath = _configuration.GetSection("UsbDevices:" + i).GetSection("DevicePath").Value;
+                        if (usbDevices[j].DevicePath == devicePath)
+                            devices.Add(usbDevices[j]);
+                    }
+                }
 
-                if (/*process != null &&*/ devices.Count > 0)
+                if (process != null && devices.Count > 0)
                 {
                     IntPtr handle = process.MainWindowHandle;
                     SetForegroundWindow(handle);
                     break;
                 }  
+                else if (devices.Count > 0 && testMode) 
+                {
+                    break;
+                }
 
                 await Task.Delay(1000);
             }
 
-            await EvaluateUsbStreams(devices); 
+            if (testMode)
+            {
+                CheckBitStream(devices[0]);
+            }
+            else
+            {
+                await EvaluateUsbStreams(devices);
+            }        
+        }
+
+        static void UserInteraction()
+        {
+            // Give hint to reset all Buttons
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Warning: Before starting the game please turn of all buttons!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            ConsoleKey response;
+            do
+            {
+                Console.Write("Confirm by pressing \"y\": ");
+                response = Console.ReadKey(false).Key;
+                if (response != ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                }
+            } while (response != ConsoleKey.Y);
+            Console.WriteLine();
+
+            // Ask if game should be started
+            Console.Write("Start game automatically by pressing \"y\": ");
+            response = Console.ReadKey(false).Key;
+
+            if (response == ConsoleKey.Y)
+                Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\bin\win_x64\eurotrucks2");
+
+            Console.WriteLine();
         }
 
         /// <summary>
@@ -178,7 +230,6 @@ namespace ETSSimulator
             int bytesRead, byteNo, bitNo;
             BitArray bits, compareBits;
             string configValue;
-            ScanCode scanCode;
 
             using (var stream = device.Open())
             {
@@ -188,56 +239,93 @@ namespace ETSSimulator
 
                     if (bytesRead > 0 && ready)
                     {
+                        // Set initial byte
                         byteNo = 6;
                         bits = new BitArray(new byte[] { inputBuffer[byteNo] });
                         compareBits = new BitArray(new byte[] { compareBytes[byteNo] });
 
-                        // Digital Input K1
+                        // Digital Input 0 (K1)
                         bitNo = 4;
-                        configValue = config.GetSection("DI_K1").Value;
+                        configValue = config.GetSection("DI_0").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
 
-                        if(!string.IsNullOrEmpty(configValue)) 
-                        {
-                            scanCode = (ScanCode)Enum.Parse(typeof(ScanCode), configValue);
-
-                            if (compareBits[bitNo] && !bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                            else if (!compareBits[bitNo] && bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                        }
-
-                        // Digital Input K2
+                        // Digital Input 1 (K2)
                         bitNo = 5;
-                        configValue = config.GetSection("DI_K2").Value;
+                        configValue = config.GetSection("DI_1").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
 
-                        if (!string.IsNullOrEmpty(configValue))
-                        {
-                            scanCode = (ScanCode)Enum.Parse(typeof(ScanCode), configValue);
-
-                            if (compareBits[bitNo] && !bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                            else if (!compareBits[bitNo] && bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                        }
-
-                        // Digital Input K3
+                        // Digital Input 2 (K3)
                         bitNo = 6;
-                        configValue = config.GetSection("DI_K3").Value;
+                        configValue = config.GetSection("DI_2").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
 
-                        if (!string.IsNullOrEmpty(configValue))
-                        {
-                            scanCode = (ScanCode)Enum.Parse(typeof(ScanCode), configValue);
+                        // Digital Input 3 (K4)
+                        bitNo = 7;
+                        configValue = config.GetSection("DI_3").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
 
-                            if (compareBits[bitNo] && !bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                            else if (!compareBits[bitNo] && bits[bitNo])
-                                SimulateKeyboardInput(scanCode);
-                        }                   
+                        // Change Byte
+                        byteNo = 7;
+                        bits = new BitArray(new byte[] { inputBuffer[byteNo] });
+                        compareBits = new BitArray(new byte[] { compareBytes[byteNo] });
+
+                        // Digital Input 4 (L2)
+                        bitNo = 0;
+                        configValue = config.GetSection("DI_4").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 5 (R2)
+                        bitNo = 1;
+                        configValue = config.GetSection("DI_5").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 6 (L1)
+                        bitNo = 2;
+                        configValue = config.GetSection("DI_6").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 7 (R1)
+                        bitNo = 3;
+                        configValue = config.GetSection("DI_7").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 8 (SE)
+                        bitNo = 4;
+                        configValue = config.GetSection("DI_8").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 9 (ST)
+                        bitNo = 5;
+                        configValue = config.GetSection("DI_9").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 10 (K11)
+                        bitNo = 6;
+                        configValue = config.GetSection("DI_10").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
+
+                        // Digital Input 11 (K12)
+                        bitNo = 7;
+                        configValue = config.GetSection("DI_11").Value;
+                        SimulateKeyIfDiffers(compareBits[bitNo], bits[bitNo], configValue);
                     }
 
                     compareBytes = (byte[])inputBuffer.Clone();
                     ready = true;
                 }
+            }
+        }
+
+        static void SimulateKeyIfDiffers(bool compareBit, bool bit, string key)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                ScanCode scanCode = (ScanCode)Enum.Parse(typeof(ScanCode), key);
+
+                if (compareBit && !bit)
+                    SimulateKeyboardInput(scanCode);
+                else if (!compareBit && bit)
+                    SimulateKeyboardInput(scanCode);
             }
         }
 
@@ -285,24 +373,55 @@ namespace ETSSimulator
 
         static void CheckBitStream(HidDevice device)
         {
-            using (var stream = device.Open())
-            {
-                byte[] inputBuffer = new byte[device.GetMaxInputReportLength()];
+            bool ready = false;
 
+            byte[] inputBuffer = new byte[device.GetMaxInputReportLength()];
+            byte[] compareBytes = null;
+
+            using (var stream = device.Open())
+            {           
                 while (true)
                 {
                     int bytesRead = stream.Read(inputBuffer, 0, inputBuffer.Length);
 
-                    Console.WriteLine("Bitstream:");
-
-                    for (var i = 0; i < bytesRead; i++)
+                    if(ready)
                     {
-                        var bits = Convert.ToString(inputBuffer[i], 2).PadLeft(8, '0');
-                        Console.WriteLine($"Byte{i}: {bits}");
+                        for(int i = 0; i < inputBuffer.Length; i++)
+                        {
+                            if (inputBuffer[i] != compareBytes[i])
+                            {
+                                string byteNo = i.ToString();
+                                string bitNo = "";
+
+                                BitArray bits = new BitArray(new byte[] { inputBuffer[i] });
+                                BitArray compareBits = new BitArray(new byte[] { compareBytes[i] });
+
+                                for(int j = 0; j < bits.Length; j++)
+                                {
+                                    if (bits[j] != compareBits[j])
+                                    {
+                                        bitNo = j.ToString();
+                                    }
+                                }
+
+                                if(byteNo != "3")
+                                    Console.WriteLine($"Byte: {byteNo}; Bit: {bitNo}");
+                            }
+                        }
                     }
 
-                    Console.WriteLine("");
-                    Thread.Sleep(1000);
+                    compareBytes = (byte[])inputBuffer.Clone();
+                    ready = true;
+
+                    //Console.WriteLine("Bitstream:");
+
+                    //for (var i = 0; i < bytesRead; i++)
+                    //{
+                    //    var bits = Convert.ToString(inputBuffer[i], 2).PadLeft(8, '0');
+                    //    Console.WriteLine($"Byte{i}: {bits}");
+                    //}
+
+                    //Console.WriteLine("");
                 }
             }
         }
